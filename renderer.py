75c9a5fc4be048bd0fb076bc9a4379b020402eee
spidey <adam@symmetry.ninja>
2025-02-15 21:32:28 +1100

    Created auto mode
    Auto mode uses decorators in the scad file instead of a config file, generally simplifies things a lot!

--- renderer.py
+++ renderer.py
@@ -1,70 +1,87 @@
 #!/usr/bin/env python3
-import threading, queue, subprocess, os, sys, yaml, platform, multiprocessing, getopt
+import threading, queue, subprocess, os, sys, yaml, platform, multiprocessing, re
 # from datetime.datetime 
 from datetime import datetime
+from argparse import ArgumentParser
+parser = ArgumentParser()
 
 startTime = datetime.now()
 
 # import the params file
 current_dir = os.getcwd()
-config_file = "render_config.yml"
-scad_file = "project.scad"
 
-help_text = """
-OpenSCAD threaded renderer
---------------------------
-usage: renderer.py -f <scadfile> -c <configfile>
-Params:
-    * -f (optional) : base scad file - defaults to project.scad, can be overridden by config file
-    * -c (optional) : config file - defaults to render_config.yaml
-"""
+parser = ArgumentParser()
+parser.add_argument("-f", "--file", dest="scad_file", help="OpenSCAD project filename", default="project.scad")
+parser.add_argument("-c", "--config", dest='config_file', help="Config filename", default="render_config.yml")
+parser.add_argument("-a", "--auto", dest='automated', help="Config filename", default=True)
+args = parser.parse_args()
 
-# parse arguments
-try:
-    opts, args = getopt.getopt(sys.argv[1:],"hf:c:",["scadfile=","configfile="])
-except getopt.GetoptError:
-    print (help_text)
-    sys.exit(2)
-for opt, arg in opts:
-    if opt == '-h':
-        print (help_text)
-        sys.exit()
-    elif opt in ("-f", "--scadfile"):
-        scad_file = arg
-    elif opt in ("-c", "--configfile"):
-        config_file = arg
+config_file = args.config_file
+scad_file = args.scad_file
+automated = args.automated
+models = []
 
 # some defaults
 number_of_threads = multiprocessing.cpu_count()
 quality = 95
 if platform.system() == "Windows":
-  openscad_path = 'c:/Program Files/OpenSCAD/openscad.exe'
+    openscad_path = 'c:/Program Files/OpenSCAD/openscad.exe'
 if platform.system() == "Darwin":
-  openscad_path = '/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD'
+    openscad_path = '/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD'
+if platform.system() == "Linux":
+    result = subprocess.run(['which', 'openscad'], stdout=subprocess.PIPE)
+    openscad_path = result.stdout.strip() 
 
 # read the yaml in
-with open("{}/{}".format(current_dir, config_file), 'r') as stream:
-    try:
-        render_config = yaml.load(stream, Loader=yaml.FullLoader)
-        if 'scad_file' in render_config:
-            scad_file = render_config['scad_file']
-        if 'quality' in render_config:
-            quality = render_config['quality']
-        if 'threads' in render_config:
-            number_of_threads = render_config['threads']
-        if 'openscad_path' in render_config:
-            openscad_path = render_config["openscad_path"]
-        if 'models' in render_config:
-            models = render_config["models"]
-        else:
-            raise Exception("no models to render in yaml file?")
-    except yaml.YAMLError as exc:
-        print(exc)
-        sys.exit
-    except Exception as exc:
-        print(exc)
-        sys.exit
-    
+if not automated:
+    with open("{}/{}".format(current_dir, config_file), 'r') as stream:
+        try:
+            render_config = yaml.load(stream, Loader=yaml.FullLoader)
+            if 'scad_file' in render_config:
+                scad_file = render_config['scad_file']
+            if 'quality' in render_config:
+                quality = render_config['quality']
+            if 'threads' in render_config:
+                number_of_threads = render_config['threads']
+            if 'models' in render_config:
+                models = render_config["models"]
+            else:
+                raise Exception("no models to render in yaml file?")
+        except yaml.YAMLError as exc:
+            print(exc)
+            sys.exit
+        except Exception as exc:
+            print(exc)
+            sys.exit
+else:
+    ## load the file and look for decorators
+    config_decorator = '//render'
+    file_read = open(scad_file, 'r')
+    for line in file_read.readlines():
+        if config_decorator in line:
+            ## get top level settings
+            if '//render.quality' in line: # quality
+                quality = int(line.replace(']','[').split('[')[1])
+                print('found quality: {}'.format(quality))
+            elif '//render.number_of_threads' in line: # threads
+                number_of_threads = int(line.replace(']','[').split('[')[1])
+                print('found number_of_threads: {}'.format(number_of_threads))
+            elif '//render.model.stl' in line: # stl file
+                module_name = re.search('module (.*)\\(', line).group(1)
+                print('found module {}'.format(module_name))
+                models += [dict(suffix='stl', module = module_name)]
+            elif '////render.model.svg' in line: # svg file
+                module_name = re.search('module (.*)\\(', line).group(1)
+                print('found module {}'.format(module_name))
+                models += [dict(suffix='svg', module = module_name)]
+            elif '////render.model.dxf' in line: # dxf file
+                module_name = re.search('module (.*)\\(', line).group(1)
+                print('found module {}'.format(module_name))
+                models += [dict(suffix='dxf', module = module_name)]
+            else:
+                print('unknown render line: {}'.format(line) )
+    print(models)
+
 # Rendering worker class
 class Job:
     module_name = output_format = temp_file_name = output_file_name = start = ""
@@ -126,7 +143,7 @@ for worker in range(number_of_threads):
 
 # Run threads
 for worker in worker_threads:
-        worker.setDaemon(True)
+        worker.daemon = True
         worker.start()
 
 # Tell python to wait for threads by "joining" them
